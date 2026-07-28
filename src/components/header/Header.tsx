888c66e07c7ebd44d751e26d3017be055290135b
João Gabriel <contato.johngabriel@gmail.com>
2026-07-28 14:44:57 -0300

    Feature: Header - Adicionado o menu de navegação e botão para a rota de Doações

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 
 export default function Header() {
@@ -8,6 +10,41 @@ export default function Header() {
                     <Link href="/">VivaPet</Link>
 
                     <nav className="menu">
+
+                        <div className="menu-item">
+                            <span>Animais</span>
+
+                            <div className="submenu">
+                                <Link href={"/animais/cadastrar"}>Cadastrar um animal novo</Link>
+                                <Link href={"/animais/necessidades"}>Cadastrar necessidades do animal</Link>
+                            </div>
+
+                        </div>
+
+                        <div className="menu-item">
+                            <span>Cuidadores</span>
+
+                            <div className="submenu">
+                                <Link href={"/cuidadores/cadastrar"}>Cadastrar um cuidador novo</Link>
+                                <Link href={"/cuidadores/necessidades"}>Vincular cuidador</Link>
+                            </div>
+
+                        </div>
+
+                        <div className="menu-item">
+                            <span>Suprimentos</span>
+
+                            <div className="submenu">
+                                <Link href={"/suprimentos/cadastrar"}>Cadastrar suprimentos</Link>
+                                <Link href={"/suprimentos/necessidades"}>Checar suprimentos</Link>
+                            </div>
+
+                        </div>
+
+
+                            <div className="donations">
+                                <Link href={"/doacoes"}>Doações</Link>
+                            </div>
 
                     </nav>
                 </div>
