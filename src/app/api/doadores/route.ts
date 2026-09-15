891834e60fac47570a5653b8e75c69f50fafa702
JOAO GABRIEL ROCHA DE CARVALHO <a96348439@rn.senac.local>
2026-09-15 14:33:58 -0300

    SRC atualizado com as telas Cuidadores, Doadores e Suprimentos

--- src/app/api/doadores/route.ts
+++ src/app/api/doadores/route.ts
@@ -2,10 +2,10 @@ import { DoadorController } from "@/controllers/DoadorController/DoadorControlle
 
 const doadorController = new DoadorController();
 
-export async function GET(req: Request){
+export async function GET(req: Request) {
     return doadorController.listarTodos(req);
 }
 
-export async function POST(req: Request){
+export async function POST(req: Request) {
     return doadorController.cadastrar(req);
 }
